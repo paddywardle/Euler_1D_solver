7e67643d897cfa33e98d66cb6b7688dc3d358d86
Paddy Wardle <paddy.wardle@yahoo.com>
2022-11-20 11:44:19 +0000

    solvers file updated with Richtmyer and FORCE flux functions

--- src/solvers.cpp
+++ src/solvers.cpp
@@ -41,6 +41,43 @@ double Euler1D::lax_friedrich_flux(std::array<double, 3> u_i, std::array<double,
   return fhalf;
 }
 
+double Euler1D::richtmyer_flux(std::array<double, 3> u_i, std::array<double, 3>u_iPlus1, std::array<double, 3>u_prim_i, std::array<double, 3> u_prim_iPlus1, int col, double dt)
+{
+  double uhalf_rho = 0.5 * (u_i[0] + u_iPlus1[0]) - 0.5 * (dt / dx) * (flux_fn_rho(u_iPlus1[0], u_prim_iPlus1[1]) - flux_fn_rho(u_i[0], u_prim_i[1]));
+
+  double uhalf_mom = 0.5 * (u_i[1] + u_iPlus1[1]) - 0.5 * (dt / dx) * (flux_fn_mom(u_iPlus1[0], u_prim_iPlus1[1], u_prim_iPlus1[2]) - flux_fn_mom(u_i[0], u_prim_i[1], u_prim_i[2]));
+
+  double uhalf_E = 0.5 * (u_i[2] + u_iPlus1[2]) - 0.5 * (dt / dx) * (flux_fn_E(u_iPlus1[2], u_prim_iPlus1[1], u_prim_iPlus1[2]) - flux_fn_E(u_i[2], u_prim_i[1], u_prim_i[2]));
+
+  double uhalf_v = uhalf_mom / uhalf_rho;
+
+  double uhalf_p = (uhalf_E - 0.5 * uhalf_rho * pow(uhalf_v, 2.0)) * (gamma - 1);
+
+  double fhalf;
+  
+  if (col == 0)
+    {
+      fhalf = flux_fn_rho(uhalf_rho, uhalf_v);
+    }
+  else if (col == 1)
+    {
+      fhalf = flux_fn_mom(uhalf_rho, uhalf_v, uhalf_p);
+    }
+  else if (col == 2)
+    {
+      fhalf = flux_fn_E(uhalf_E, uhalf_v, uhalf_p);
+    }
+
+  return fhalf;
+}
+
+double Euler1D::FORCE_flux(std::array<double, 3> u_i, std::array<double, 3>u_iPlus1, std::array<double, 3>u_prim_i, std::array<double, 3> u_prim_iPlus1, int col, double dt)
+{
+  double fhalf = 0.5 * (lax_friedrich_flux(u_i, u_iPlus1, u_prim_i, u_prim_iPlus1, col, dt) + richtmyer_flux(u_i, u_iPlus1, u_prim_i, u_prim_iPlus1, col, dt));
+
+  return fhalf;
+}
+
 double Euler1D::calculate_timestep()
 {
   
@@ -87,7 +124,7 @@ void Euler1D::solvers()
       {
 	for (int j=0; j<flux[i].size(); j++)
 	  {
-	    flux[i][j] = lax_friedrich_flux(u[i], u[i+1], u_prim[i], u_prim[i+1], j, dt);
+	    flux[i][j] = FORCE_flux(u[i], u[i+1], u_prim[i], u_prim[i+1], j, dt);
 	  }
       }
    
